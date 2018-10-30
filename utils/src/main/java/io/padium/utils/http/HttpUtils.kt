4710600f1e8bb444ce6fbaad4f3c119c3a932964
Hayden James <hayden.james@gmail.com>
2018-10-29 23:53:10 -0400

    Added support for HTTP streaming and maintaining a persistent connection

--- utils/src/main/java/io/padium/utils/http/HttpUtils.kt
+++ utils/src/main/java/io/padium/utils/http/HttpUtils.kt
@@ -4,14 +4,43 @@ import okhttp3.MediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody
+import okio.BufferedSink
 import org.apache.commons.codec.net.URLCodec
+import java.io.OutputStream
 import java.lang.StringBuilder
+import java.util.concurrent.TimeUnit
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSocketFactory
+import javax.net.ssl.TrustManagerFactory
+import javax.net.ssl.X509TrustManager
 
 enum class HttpMethod {
     GET, HEAD, POST, OPTIONS, PUT, PATCH, DELETE
 }
 
 object HttpUtils {
+    private val client : OkHttpClient
+
+    init {
+        val builder = OkHttpClient.Builder()
+        builder.connectTimeout(0, TimeUnit.SECONDS)
+        builder.readTimeout(0, TimeUnit.SECONDS)
+        builder.writeTimeout(0, TimeUnit.SECONDS)
+        //val tlsConfig = buildTlsFactory()
+        //builder.sslSocketFactory(tlsConfig.first, tlsConfig.second)
+        //TODO: Add client side TLS
+        client = builder.build()
+    }
+
+    @JvmStatic
+    private fun buildTlsFactory(): Pair<SSLSocketFactory, X509TrustManager> {
+        val sslContext = SSLContext.getInstance("TLS")
+        val tmf = TrustManagerFactory.getInstance("TLS")
+        val x509TrustManager = tmf.trustManagers[0] as X509TrustManager
+
+        return Pair<SSLSocketFactory, X509TrustManager>(sslContext.socketFactory,
+                x509TrustManager)
+    }
 
     @JvmStatic
     fun isInformational(code: Int): Boolean {
@@ -62,7 +91,7 @@ object HttpUtils {
     }
 
     @JvmStatic
-    fun requestText(url: String, method: HttpMethod, contentType: String, requestBody: String = "",
+    fun requestText(url: String, method: HttpMethod, contentType: String = "", requestBody: String = "",
                     requestHeaders : Map<String, String> = mapOf()): Triple<Int, String?, Map<String, List<String>>> {
         val mediaType  = MediaType.parse(contentType)
         val body = RequestBody.create(mediaType, requestBody)
@@ -78,8 +107,34 @@ object HttpUtils {
     }
 
     @JvmStatic
+    fun requestBinary(url: String, method: HttpMethod, contentType: String, requestBody: ByteArray,
+                      offset: Int, byteCount: Int, requestHeaders : Map<String, String> = mapOf()):
+            Triple<Int, String?, Map<String, List<String>>> {
+        val mediaType  = MediaType.parse(contentType)
+        val body = RequestBody.create(mediaType, requestBody, offset, byteCount)
+        return request(url, method, body, requestHeaders)
+    }
+
+    @JvmStatic
+    fun requestBinary(url: String, method: HttpMethod, contentType: String,
+                      streamer: (stream: OutputStream) -> Unit,
+                      requestHeaders : Map<String, String> = mapOf()):
+            Triple<Int, String?, Map<String, List<String>>> {
+         val body = object : RequestBody() {
+            override fun contentType(): MediaType? {
+                return MediaType.parse(contentType)
+            }
+            override fun writeTo(sink: BufferedSink) {
+                streamer(sink.outputStream())
+            }
+        }
+        return request(url, method, body, requestHeaders)
+    }
+
+    @JvmStatic
     private fun request(url: String, method: HttpMethod, requestBody: RequestBody,
-                        requestHeaders : Map<String, String>): Triple<Int, String?, Map<String, List<String>>> {
+                        requestHeaders : Map<String, String>):
+            Triple<Int, String?, Map<String, List<String>>> {
         //Build request with method
         val request = Request.Builder().url(url)
         when(method) {
@@ -96,7 +151,6 @@ object HttpUtils {
         for(requestHeader in requestHeaders)
             request.addHeader(requestHeader.key, requestHeader.value)
 
-        val client = OkHttpClient()
         val response = client.newCall(request.build()).execute()
 
         val headersMap = mutableMapOf<String, List<String>>()
